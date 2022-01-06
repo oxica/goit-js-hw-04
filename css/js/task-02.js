8c5e9b6dd3349c4aff3da3c800b2cacab873eb42
oxica <kskaplun@gmail.com>
2022-01-06 22:22:21 +0200

    add

--- css/js/task-02.js
+++ css/js/task-02.js
@@ -1,5 +1,32 @@
 
-
-
-
-
+const inventory = {
+    items: ['Knife', 'Gas mask'],
+    add(itemName) {
+      console.log(`Adding ${itemName} to inventory`);
+  
+      this.items.push(itemName);
+    },
+    remove(itemName) {
+      console.log(`Removing ${itemName} from inventory`);
+  
+      this.items = this.items.filter(item => item !== itemName);
+    },
+  };
+  
+  const invokeInventoryAction = function(itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    action(itemName);
+  };
+  
+  
+  invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+  // Invoking action on Medkit
+  // Adding Medkit to inventory
+  
+  console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+  
+  invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+  // Invoking action on Gas mask
+  // Removing Gas mask from inventory
+  
+  console.log(inventory.items); // ['Knife', 'Medkit']
